3fa6140ccc821479b2fd601ed82633596e212398
Danny Ou <idannyou@gmail.com>
2018-12-26 11:32:22 -0800

    Move public files to root

---
+++ precache-manifest.693a2a6dc8cba812960f3f80d8d9167a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dcaf3514722f56279db",
+    "url": "/static/css/main.ea928faf.chunk.css"
+  },
+  {
+    "revision": "7dcaf3514722f56279db",
+    "url": "/static/js/main.7dcaf351.chunk.js"
+  },
+  {
+    "revision": "20ae03e4b61a1491b8bf",
+    "url": "/static/js/1.20ae03e4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b9295b8e54fafc3198de7e01d274d04",
+    "url": "/index.html"
+  }
+];
